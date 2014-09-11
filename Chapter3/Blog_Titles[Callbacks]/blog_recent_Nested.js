8f594f3233188c826f3327a562c74895abd1c319
Sean Robinson <Sean.Robinson@LexAble.com>
2014-09-11 15:34:20 +0100

    Added callback examples

---
+++ Chapter3/Blog_Titles[Callbacks]/blog_recent_Nested.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    if (req.url === '/') {
+        fs.readFile('./titles.json', function(err, data) {
+            if (err) {
+                console.error(err);
+                res.end('Server Error');
+            } else {
+                var titles = JSON.parse(data.toString());
+
+                fs.readFile('./template.html', function(err, data) {
+                    if (err) {
+                        console.error(err);
+                        res.end('Server error');
+                    } else {
+                        var templ = data.toString(),
+                            html = templ.replace('%', titles.join('</li><li>'));
+                        res.writeHead(200, {'content-type': 'text/html'});
+                        res.end(html);
+                    }
+                });
+            }
+        });
+    }
+}).listen(8000, '127.0.0.1');
